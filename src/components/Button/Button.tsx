2e523d858d4efd5f2be18b1948c055bf9425f80e
Rose <rosesophiewright@gmail.com>
2022-06-18 14:47:00 -0500

    storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import "./Button.css";
+import React from "react";
+import "./Button.scss";
 
-interface ButtonProps {
-    lable: string;
+export interface ButtonProps {
+  label: string;
 }
 
 const Button = (props: ButtonProps) => {
-    return <button>{props.lable}</button>
-}
+  return <button>{props.label}</button>;
+};
 
-export default Button
+export default Button;
